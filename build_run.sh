e631ea84d861d264248f0a9043f673d050c679b9
dqn <iamdqncoder@gmail.com>
2024-07-23 00:23:09 +0800

    build shell

--- build_run.sh
+++ build_run.sh
@@ -3,4 +3,4 @@ docker stop igpsport2xingzhe
 docker rm igpsport2xingzhe
 docker rmi igpsport2xingzhe:v1
 docker build -t igpsport2xingzhe:v1 .
-docker run -d --name igpsport2xingzhe -v /root/apps/igpsport2xingzhe-java/data:/data -p 18877:18877 igpsport2xingzhe:v1
+docker run -d --name igpsport2xingzhe -v ./data:/data -p 18877:18877 igpsport2xingzhe:v1
